e3c030468868ed3eb88d1d74b0ff23216f6bf96c
Radkovets Ilya <Radkovets.ilya@gmail.com>
2024-03-06 11:43:06 +0300

    add Lesson1:Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 12;
+        int b = 3;
+        int c = 8;
+        int d = 67;
+        int e = 34;
+
+        int max = a;
+
+        if (b > max)
+        {
+            max = b;
+        }
+        if (c > max)
+        {
+            max = c;
+        }
+        if (d > max)
+        {
+            max = d;
+        }
+        if (e > max)
+        {
+            max = e;
+        }
+        Console.WriteLine (max);
+    }
+}
+
