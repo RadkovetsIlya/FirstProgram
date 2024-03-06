ad03611e06f82fd96410017401df7ac93049daf4
Radkovets Ilya <Radkovets.ilya@gmail.com>
2024-03-06 10:32:21 +0300

    Добавили переменные

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hellow world");
+﻿// Console.WriteLine("Hellow world");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
