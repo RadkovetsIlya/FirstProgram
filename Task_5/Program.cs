7f604ae4dce487706a97d96c91235ea53fd08ba9
Radkovets Ilya <Radkovets.ilya@gmail.com>
2024-03-06 12:10:57 +0300

    add Lesson1:Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 100;
+        int i = 1;
+        while (i <= n)
+        {
+            //Console.WriteLine(i);
+            Console.Write(i);
+            Console.Write(' ');
+            i = i + 1;
+        }
+    }
+}
