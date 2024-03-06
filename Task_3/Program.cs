639c5f7ae8a8271949ca8ac3166cbfb7666e688c
Radkovets Ilya <Radkovets.ilya@gmail.com>
2024-03-06 11:15:54 +0300

    Добавили сравнение

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 12;
+        int b = 3;
+
+        if (a > b)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
